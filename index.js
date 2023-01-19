8e67b5baad89accee3fd8cabd52eae9570342d49
RanganaDinesh <118040042+RanganaDinesh@users.noreply.github.com>
2023-01-19 21:50:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// "Guess the Output
+
+// function FindSum(a, b){
+//     // return a + b;
+//     console.log(a+b)
+// }
+
+// function DisplayData(data, batch){
+//     console.log(`i am from ${data} and My batch is EA${batch}`)
+// }
+
+// DisplayData("PrepBytes", FindSum(10, 9)); 
+// "Guess the output
+
+// Abc();
+// const Abc = function(){
+//     let value = 20;
+//     console.log(value);
+// }
+// output: ReferenceError: Cannot access 'Abc' before initialization
